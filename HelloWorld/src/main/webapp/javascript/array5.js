acb6d72621c9ebb62d3b39a49d015465f6e1fa24
박소연 <@DESKTOP-C312479>
2021-11-01 20:38:37 +0900

    ..

--- HelloWorld/src/main/webapp/javascript/array5.js
+++ HelloWorld/src/main/webapp/javascript/array5.js
@@ -16,19 +16,30 @@ const users = [new User('user1', '사용자', 90),
 // 표형식(table)으로 생성.
 document.write('<div id="show"></div>');
 document.write('<button onclick="createContent()">생성</button>');
+
 // <ul><li>아이디, 이름, 점수</li><li>아이디, 이름, 점수</li></ul>
-
 let table = document.createElement('table');
+table.border = "1";
 let thead = document.createElement('thead');
+let tr = document.createElement('tr');
+for(let field in users[0]) {
+    let th = document.createElement('th');
+    th.innerHTML = field;
+    tr.appendChild(th);
+}
+thead.appendChild(tr);
+table.appendChild(thead);
 
-function createContent() {
-    let trTag = document.createElement('tr');
-    let tdTag = document.createElement('td');
-
-    for (var i = 0; i < users.length; i++) {
-      trTag.innerHTML = 
-
+let tbody = document.createElement('tbody');
+for(let i = 0; i < users.length; i++) {
+    let tr = document.createElement('tr');
+    for(let field in users[i]) {
+        let td = document.createElement('td');
+        td.innerHTML = users[i][field];
+        tr.appendChild(td);
     }
-    document.getElementById("show").appendChild();
-
-};
+    tbody.appendChild(tr);
+}
+table.appendChild(tbody);
+console.log(table);
+document.getElementById("show").appendChild(table);
